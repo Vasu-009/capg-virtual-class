9bc8412aae7be891d13d0542d79ec690b4ed11bb
Revanth <Revanth@Revanth>
2020-04-11 15:30:58 +0530

    commit

---
+++ capg-movie-info/src/main/java/com/capg/movieinfo/controller/MovieInfoController.java
@@ -0,0 +1,39 @@
+package com.capg.movieinfo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.capg.movieinfo.model.MovieInfo;
+import com.capg.movieinfo.service.MovieInfoService;
+
+@RestController
+@RequestMapping("/info")
+public class MovieInfoController {
+	
+	@Autowired
+	MovieInfoService service;
+	
+	@GetMapping("/id/{id}")
+	public MovieInfo getMovieInfo(@PathVariable long id) {
+		return service.getMovieInfo(id);
+	}
+
+	@GetMapping("/all")
+	public List<MovieInfo> getAllMovieInfos(){
+		return service.getAllMovieInfo();
+	}
+	
+	@PostMapping("/add")
+	public MovieInfo addMovieInfo(MovieInfo movie) {
+		return service.addMovieInfo(movie);
+		
+	}
+}
+
---
+++ capg-movie-info/src/main/java/com/capg/movieinfo/repo/MovieInfoRepo.java
@@ -0,0 +1,13 @@
+package com.capg.movieinfo.repo;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.capg.movieinfo.model.MovieInfo;
+
+public interface MovieInfoRepo extends JpaRepository<MovieInfo, Long>{
+
+	
+
+}
