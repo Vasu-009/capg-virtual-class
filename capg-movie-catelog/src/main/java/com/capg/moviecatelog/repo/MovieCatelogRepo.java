0ae399a194c28ef2e21fddc87ef4c50d48915621
Revanth <Revanth@Revanth>
2020-04-11 20:50:58 +0530

    commit

--- capg-movie-catelog/src/main/java/com/capg/moviecatelog/repo/MovieCatelogRepo.java
+++
@@ -1,9 +0,0 @@
-package com.capg.moviecatelog.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capg.moviecatelog.model.MovieCatelog;
-
-public interface MovieCatelogRepo extends JpaRepository<MovieCatelog, Long> {
-
-}
