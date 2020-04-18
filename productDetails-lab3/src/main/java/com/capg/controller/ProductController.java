c32c0b7a475c5ea612b359329b65c7b42b52ea11
Revanth <Revanth@Revanth>
2020-04-18 15:42:34 +0530

    commit

--- productDetails-lab3/src/main/java/com/capg/controller/ProductController.java
+++ productDetails-lab3/src/main/java/com/capg/controller/ProductController.java
@@ -4,32 +4,56 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capg.model.Product;
 import com.capg.service.ProductService;
 
 @RestController
-//@CrossOrigin(origins="*")
+@RequestMapping("/api")
 public class ProductController {
 
 	@Autowired
 	ProductService  productService;
 	
-	@GetMapping("product")
-	public List<Product>  getAllProducts()
+	@GetMapping("/all")
+	public List<Product>  findAll()
 	{
-		  return productService.findAllProducts();
+		  return productService.findAll();
+	}
+
+	@GetMapping("/product/id/{id}")
+	public Product getOne(@PathVariable int id) 
+	{
+		  return productService.getOne(id);
 	}
 	
-	@PostMapping("product")
-	public Product  createProduct(@RequestBody Product product)
+	@PostMapping("/add")
+	public Product  save(@RequestBody Product product)
 	{
-		return  productService.createProduct(product);
+		return  productService.save(product);
 	}
+	
+	@DeleteMapping("/product/id/{id}")
+	public boolean  deleteById(@PathVariable int id)
+	{
+		return  productService.deleteById(id);
+	}
+
+	@PutMapping("/product")
+	public Product  updateProduct(@RequestBody Product product)
+	{
+		return  productService.updateProduct(product);
+	}
+	
+	
 	
 	
 }
---
+++ productDetails-lab3/src/main/java/snippet/Snippet.java
@@ -0,0 +1,9 @@
+package snippet;
+
+public class Snippet {
+	{
+	    "productId":0,
+	    "productName": null,
+	    "price": 0.0
+	}
+}
